89303f5b2ce79091634884413209f417e42e79e1
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2022-08-19 01:30:08 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-    description = "The public IP address assigned to the instance, if applicable.
-    value = "${join(",", aws_instance.testInstance.*.public_ip, "")}"
+  description = "The public IP address assigned to the instance, if applicable."
+  value       = try(aws_instance.testInstance.1.public_ip, aws_instance.testInstance.2.public_ip, "")
 }
