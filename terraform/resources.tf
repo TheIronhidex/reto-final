3f6076a728f31cab47eff0e2c5c96d3030371933
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2022-08-19 02:05:41 +0200

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -82,5 +82,7 @@ resource "aws_instance" "testInstance" {
   instance_type          = "${var.instance_type}"
   subnet_id              = "${aws_subnet.subnet_public.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  key_name               = "jose-ssh" 
+  key_name               = "jose-ssh"
+  tags = {
+    name  = element(var.instance_tags, count.index)
 }
