a13ba91d8fd68bfe55ef3e5a32e0c5b4dc445ccf
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2022-08-19 16:41:40 +0200

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -9,8 +9,8 @@ provider "aws" {
 ##Create vpc
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.cidr_vpc}"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 ##Create gateway
