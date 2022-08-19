fc662a9d93516f59921e3e80a94f6be73a205014
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2022-08-19 03:33:08 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
   description = "The public IP address assigned to the app server, if applicable."
-  value       = values(aws_instance.app.public_ip, aws_instance.web.public_ip)  
+  value       = split(",", [aws_instance.app.public_ip, aws_instance.web.public_ip])  
 }
