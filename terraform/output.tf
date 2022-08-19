869ad59487df1e4a2232a273aa001da6d3a06985
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2022-08-19 02:40:37 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,8 @@
-output "public_ip" {
-  description = "The public IP address assigned to the instance, if applicable."
-  value       = [try(aws_instance.testInstance.*.public_ip), try(aws_instance.testInstance.*.public_ip)]
+output "public_ip_app" {
+  description = "The public IP address assigned to the app server, if applicable."
+  value       = try(aws_instance.app_server.public_ip, "")
+}
+output "public_ip_web" {
+  description = "The public IP address assigned to the app server, if applicable."
+  value       = try(aws_instance.web_server.public_ip, "")
 }
